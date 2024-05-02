e82b61d798b597a24205ea20a6c6f8ab3932e5be
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-05-02 19:32:09 +0200

    Finish up 404 logic

--- index.js
+++ index.js
@@ -12,9 +12,26 @@ const server = http.createServer((req, res) => {
     'index.html' : req.url)
 
     fs.readFile(filePath, (err, content) => {
-        res.writeHead(200, { 'Content-Type': 'text/html'})
-        res.end(content, 'utf8')
+        if (err) {
+            if(err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html'});
+                    res.end(content, 'utf8')
+                })
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`)
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.end(content, 'utf8')
+        }
+
     })
+
+
 })
 
-server.listen(8080)
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
